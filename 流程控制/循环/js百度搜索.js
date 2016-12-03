5aa08add28c9fc1226a8d516d958cfcb84803847
李志伟 <1548790965@qq.com>
2016-12-03 16:23:24 +0800

    上传最新

---
+++ 流程控制/循环/js百度搜索.js
@@ -0,0 +1,4 @@
+search1="python"
+search2="c语言"
+search3="github"
+search4="seIDE"
---
+++ 流程控制/循环/js脑图登录.js
@@ -0,0 +1,23 @@
+var locatorLZW = {
+	userName: "id=login_email",
+	passWord: "id=login_password",
+	btnName: "id=signin_btn",
+	loginName: "css=span.button",
+};
+
+userName1="wangding@chmiot.net"
+passWord1="Passw0rd"
+assert1="登录 | ProcessOn"
+verifyTarget1="javascript{locatorLZW.btnName}"
+verifyValue1="立即登录"
+userName2="wangding@chmiot.ne"
+passWord2="Passw0rd!"
+assert2="登录 | ProcessOn"
+verifyTarget2="javascript{locatorLZW.btnName}"
+verifyValue2="立即登录"
+userName3="wangding@chmiot.net"
+passWord3="Passw0rd!"
+assert3="ProcessOn - 我的文件"
+verifyTarget3="javascript{locatorLZW.loginName}"
+verifyValue3="新建文件"
+
