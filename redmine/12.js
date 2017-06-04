4f81dbaa35660f76e5bb2a23fde5fa2ef571296b
lizhiwei <1548790965@qq.com>
2017-06-04 12:30:29 +0800

    上传代码

---
+++ redmine/12.js
@@ -0,0 +1,7 @@
+var locatorLZW = {
+	userName: "id=loginAccount",
+	passWord: "id=loginPassword",
+	denglu: "//button[@type='submit']",
+	chenggong: "css=img.navbar-brand-logo",
+	shibai: "css=div.layui-layer-content.layui-layer-padding",
+};
